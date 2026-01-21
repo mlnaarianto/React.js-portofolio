23f26bd6ad24b2dd6b07fa709edd039a237253e7
Maulanaaaaaaaa <maulanaarianto592@gmail.com>
2026-01-21 10:49:56 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: mode === 'production'
-    ? '/react.js-portofolio/'
+    ? '/React.js-portofolio/'
     : '/',
 }))
